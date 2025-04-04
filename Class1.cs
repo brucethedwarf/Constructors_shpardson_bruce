102766ca8a7d4642e4cf0b1245c797f15b96f847
brucethedwarf <bruceleeshepardson@gmail.com>
2025-04-04 14:32:46 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_shpardson_bruce
+{
+    internal class Game
+    {
+        public string title, genre;
+
+        public Game(string titleparam = "unknown Title", string genreparam = "casial")
+        {
+            title = titleparam;
+            genre = genreparam;
+        }
+            
+
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Constructors_shpardson_bruce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game myGame = new Game("CS:GO", "FPS");
+
+            Console.WriteLine($"MY favorite game is {myGame.title} and it is a {myGame.genre} game!");
+            
+            Game myOtherGame = new Game();
+            
+            Console.WriteLine($"My favorite game is {myOtherGame.title} and its is rated {myOtherGame.genre} game!");
+
+
+
+        }
+    }
+}
